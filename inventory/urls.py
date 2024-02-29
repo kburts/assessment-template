9be68cf162fffeaf931c679ddc0f0988879634b3
Kevin Burton <kevin.burton2@gmail.com>
2024-02-29 11:42:21 -0800

    Update urls.py

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from .views import OrderViewSet
+
+router = DefaultRouter()
+router.register(r'orders', views.OrderViewSet, basename='snippet')
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
